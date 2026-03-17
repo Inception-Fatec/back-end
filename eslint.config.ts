6acdef46acdeeff534a7f85a6ce41d8f7a1e7280
pedro-h-martins <pedrohenrimartinss@gmail.com>
2026-03-17 16:57:49 -0300

    ci: Make ci workflow using Ubuntu LTS, Node LTS, ESLint and Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["node_modules/", "package.json", "package-lock.json"] },
+   { ignores: ["node_modules/", "package.json", "package-lock.json"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
